5888dd73042a2236d9f3a1d9cccbf1e8bcd3004d
Omar <mohammed.omar078@gmail.com>
2026-02-12 14:10:07 -0800

    Initial Commit

---
+++ src/models/student.ts
@@ -0,0 +1,8 @@
+export class Student {
+  constructor(
+    public studentId: number,
+    public firstName: string,
+    public lastName: string,
+    public school: string,
+  ) {}
+}
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,12 @@
+type AboutPageProps = {
+  message: string;
+};
+const AboutPage = ({ message }: AboutPageProps) => {
+  return (
+    <section>
+      <h3>{message}</h3>
+    </section>
+  );
+};
+
+export default AboutPage;
